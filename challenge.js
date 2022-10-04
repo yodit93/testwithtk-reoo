362fa5608c7689494ba09f54ad7dc99c9b7311d8
Reggeditt <tkodjosarso@gmail.com>
2022-10-03 19:25:45 -0700

    control commit 1

--- challenge.js
+++ challenge.js
@@ -1,25 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(){
-  addWavyUnderline(findAllUnorderedListElements());
+  // addWavyUnderline(findAllUnorderedListElements());
 });
 
 function addWavyUnderline(elements) {
   //elements = document.querySelectorAll("#education ol li");
-  for (let i = 0; i < elements.length; i++){
-    elements[i].style.textDecoration = "wavy underline";
+  // for (let i = 0; i < elements.length; i++){
+  //   elements[i].style.textDecoration = "wavy underline";
   }
 }
 
 function displayProjects() {
-  //document.getElementById("projects").innerHTML.appendChild(prepareProjects(arguments));
-  
-  
-  //let y = document.createTextNode("test to be seen");
-  //x.appendChild(y);
-  let c = document.getElementById("projects");
-  let x = document.createElement("p");
-  let z = document.appendChild("p");
-  let y = document.createTextNode("test to be seen");
-  x.appendChild(y);
+ 
+  // let c = document.getElementById("projects");
+  // let x = document.createElement("p");
+  // let z = document.appendChild("p");
+  // let y = document.createTextNode("test to be seen");
+  // x.appendChild(y);
 }
 
 function findAllUnorderedListElements() {
